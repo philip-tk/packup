31b716beeb3548bf2e328fc4d60d8633d1b2c556
Philip Thomas Kanianthara <philip.kanianthara@gmail.com>
2020-05-08 23:23:50 +0800

    Added last directory management

--- packup.sh
+++ packup.sh
@@ -6,8 +6,15 @@ dest_port=$(grep port .packuprc | cut -f 2 -d\ )
 dest_dir=$(grep dest-dir .packuprc | cut -f 2 -d\ )
 src_dir=$(grep src-dir .packuprc | cut -f 2 -d\ )
 
-# DTG stands for date-time-group
+# DTG stands for date-time-group.
 # This formats the date as YYYY-MM-DD.
 DTG=$(date +%Y-%m-%d)
 
-echo $dest_dir$DTG > $dest_dir/last.dir
+# This uses the last.dir file that is populated at the end of the script to
+# get the name of the last backup directory.
+LASTBACKUP=$(cat $dest_dir/last.dir)
+
+# This line puts the file name of the last backup in a file called last.dir
+# which means it can then be used by the LASTBACKUP variable at the start
+# of the script. It is meant to be at the end of the shellscript.
+echo $dest_dir/$(echo $src_dir | grep -o '[^/]*$')-$DTG > $dest_dir/last.dir
